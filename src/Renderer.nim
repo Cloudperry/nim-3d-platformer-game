497f10a51a5a43ed06b72393637da47bf0cd8c83
Roni Hokkanen <roni.hokkanen@protonmail.com>
2025-09-13 02:42:35 +0300

    Add model transforms, keyboard controls for translation and OpenGL debug logging

--- src/Renderer.nim
+++ src/Renderer.nim
@@ -7,8 +7,14 @@ import GlUtils, Slangc
 type
   ColoredVertex = object
     pos, color: Vec3f
-  ColorUbo = object
+  # Packed pragma might be necessary or more correct for structs passed to GPU?
+  Uniforms = object
+    mvp: Mat4f
     myColor: GLfloat
+  Transform = object
+    pos: Vec3[float]
+    scale: Vec3[float]
+    rotation: Vec3[float]
 
 const shadersDir = currentSourcePath().parentDir().parentDir()
 
@@ -23,31 +29,81 @@ opts = SlangcOptions(
 let fragmentShaderText = compileShaderOrRaise(opts)
 
 var
-  vertices = @[
-    ColoredVertex(pos: vec3f(0.5, 0.5, 0.0), color: vec3f(1.0, 0.0, 0.0)), # top right
-    ColoredVertex(pos: vec3f(0.5, -0.5, 0.0), color: vec3f(0.0, 1.0, 0.0)), # bottom right
-    ColoredVertex(pos: vec3f(-0.5, -0.5, 0.0), color: vec3f(0.0, 0.0, 1.0)), # bottom left
-    ColoredVertex(pos: vec3f(-0.5,  0.5, 0.0), color: vec3f(0.0, 1.0, 1.0)) # top left
-  ]
-  indices: seq[GLuint] = @[
-    0, 1, 3,
-    1, 2, 3
-  ]
+  # Renderer state and wrapper objects
   shader: ShaderRef
-  uniforms: ShaderStorageRef[ColorUbo]
+  uniforms: ShaderStorageRef[Uniforms]
   vbo: VertexBufferRef[ColoredVertex]
   vao: VertexArrayRef
   ebo: ElementBufferRef
 
-proc keyCb(win: Window, key: Key, scanCode: int32, action: KeyAction, modKeys: set[ModifierKey]) =
-  if key == keyEscape and action == kaDown:
-    win.shouldClose = true
+  # Model data
+  vertices = @[
+    ColoredVertex(pos: vec3f(0.5, 0.5, 0.0), color: vec3f(1.0, 0.0, 0.0)), # front top right
+    ColoredVertex(pos: vec3f(0.5, -0.5, 0.0), color: vec3f(0.0, 1.0, 0.0)), # front bottom right
+    ColoredVertex(pos: vec3f(-0.5, -0.5, 0.0), color: vec3f(0.0, 0.0, 1.0)), # front bottom left
+    ColoredVertex(pos: vec3f(-0.5,  0.5, 0.0), color: vec3f(0.0, 1.0, 1.0)), # front top left
+    ColoredVertex(pos: vec3f(0.5, 0.5, 1.0), color: vec3f(1.0, 0.0, 0.0)), # back top right
+    ColoredVertex(pos: vec3f(0.5, -0.5, 1.0), color: vec3f(0.0, 1.0, 0.0)), # back bottom right
+    ColoredVertex(pos: vec3f(-0.5, -0.5, 1.0), color: vec3f(0.0, 0.0, 1.0)), # back bottom left
+    ColoredVertex(pos: vec3f(-0.5,  0.5, 1.0), color: vec3f(0.0, 1.0, 1.0)) # back top left
+  ]
+  indices: seq[GLuint] = @[
+    # front
+    0, 1, 3,  1, 2, 3,
+    # right
+    0, 4, 1,  1, 4, 5,
+    # left
+    3, 2, 7,  2, 6, 7,
+    # bottom
+    1, 5, 2,  2, 5, 6,
+    # top
+    0, 3, 4,  3, 7, 4,
+    # back
+    4, 7, 5,  5, 7, 6
+  ]
+  modelTransform = Transform(pos: vec3d(0, 0, 0), scale: vec3d(1, 1, 1))
 
-proc init() =
+proc getTransformMat(t: Transform): Mat4f =
+  var scaleMat, translateMat, rotateMat = mat4f(1.0)
+  # Scaling
+  scaleMat[0, 0] = t.scale.x
+  scaleMat[1, 1] = t.scale.y
+  scaleMat[2, 2] = t.scale.z
+
+  # Rotation
+  let (cx, cy, cz) = (cos(t.rotation.x), cos(t.rotation.y), cos(t.rotation.z))
+  let (sx, sy, sz) = (sin(t.rotation.x), sin(t.rotation.y), sin(t.rotation.z))
+  rotateMat[0,0] = cy * cz
+  rotateMat[0,1] = -cy * sz
+  rotateMat[0,2] = sy
+  rotateMat[1,0] = sx * sy * cz + cx * sz
+  rotateMat[1,1] = -sx * sy * sz + cx * cz
+  rotateMat[1,2] = -sx * cy
+  rotateMat[2,0] = -cx * sy * cz + sx * sz
+  rotateMat[2,1] = cx * sy * sz + sx * cz
+  rotateMat[2,2] = cx * cy
+
+  # Translation
+  translateMat[3, 0] = t.pos.x
+  translateMat[3, 1] = t.pos.y
+  translateMat[3, 2] = t.pos.z
+  return translateMat * rotateMat * scaleMat
+
+proc init(win: Window, cfg: OpenglWindowConfig) =
+  if cfg.debugContext:
+    setupGlDebugLogging()
+
+  # Starting window parameters
+  win.pos = (150, 100)
+  win.aspectRatio = (3, 2)
+  win.sizeLimits = (300, 200, 1200, 800)
+
+  # Compile and link shader and check errors
   shader = initShaderProg(vertexShaderText, fragmentShaderText)
   # Get used uniforms/attributes. Bare uniforms don't work in Slang so this uses UBOs.
-  uniforms = initShaderStorage[ColorUbo](shader, GL_DYNAMIC_DRAW, 0)
+  uniforms = initShaderStorage[Uniforms](shader, GL_DYNAMIC_DRAW, 0)
 
+  # Set up OpenGL buffers for passing vertex data to shaders
   vbo = initVertexBuffer (vertices, GL_STATIC_DRAW).some
   ebo = initElementBuffer (indices, GL_STATIC_DRAW).some
   vao = initVertexArray()
@@ -56,30 +112,51 @@ proc init() =
   vao.attachElementBuffer(ebo)
 
 proc uninit() =
-  vao.cleanup() # TODO: Test if messy VAO abstraction explodes by calling vbo.cleanup or ebo.cleanup before this
+  vbo.cleanup()
+  ebo.cleanup()
+  vao.cleanup()
   uniforms.cleanup()
   shader.cleanup()
+
+proc update(win: Window, deltaTime: float) =
+  if win.isKeyDown(keyComma):
+    modelTransform.pos.z -= deltaTime * 1
+  elif win.isKeyDown(keyO):
+    modelTransform.pos.z += deltaTime * 1
+  elif win.isKeyDown(keyE):
+    modelTransform.pos.x += deltaTime * 1
+  elif win.isKeyDown(keyA):
+    modelTransform.pos.x -= deltaTime * 1
+  elif win.isKeyDown(keySpace):
+    modelTransform.pos.y += deltaTime * 1
+  elif win.isKeyDown(keyBackslash):
+    modelTransform.pos.y -= deltaTime * 1
+
+proc keyCb(win: Window, key: Key, scanCode: int32, action: KeyAction, modKeys: set[ModifierKey]) =
+  if key == keyEscape and action == kaDown:
+    win.shouldClose = true
 
 proc draw(win: Window) =
   var (width, height) = glfw.framebufferSize(win)
   var ratio = width / height
 
   glViewport(0, 0, width, height)
+  let viewMat = ortho[GLfloat](-ratio, ratio, -1.0, 1.0, 1.0, -1.0)
   glClearColor(0.2, 0.3, 0.3, 1.0)
   glClear(GL_COLOR_BUFFER_BIT)
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
   shader.use()
-  uniforms.uploadField(uniforms.data, myColor):
-    uniforms.data.myColor = sin(getTime()) / 2.0 + 0.5
+  uniforms.data.myColor = sin(getTime()) / 2.0 + 0.5
+  uniforms.data.mvp = viewMat * modelTransform.getTransformMat()
+  uniforms.upload()
   uniforms.use(shader)
   vao.use()
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, cast[pointer](0))
+  glDrawElements(GL_TRIANGLES, indices.len, GL_UNSIGNED_INT, cast[pointer](0))
 
 proc main() =
   glfw.initialize()
-
   var cfg = DefaultOpenglWindowConfig
   cfg.size = (w: 640, h: 480)
   cfg.title = "Simple example"
@@ -87,23 +164,24 @@ proc main() =
   cfg.version = glv46
   cfg.forwardCompat = true
   cfg.profile = opCoreProfile
+  # Setting to true enables debug logging
+  cfg.debugContext = true
 
   var win = newWindow(cfg)
-
   win.keyCb = keyCb
 
   if not gladLoadGL(getProcAddress):
     quit "Error initialising OpenGL"
 
   glfw.swapInterval(1)
+  init(win, cfg)
 
-  init()
-
-  win.pos = (150, 100)
-  win.aspectRatio = (3, 2)
-  win.sizeLimits = (300, 200, 1200, 800)
-
+  var prevFrameStart = getTime()
   while not win.shouldClose:
+    let currFrameStart = getTime()
+    let deltaTime = currFrameStart - prevFrameStart
+    prevFrameStart = currFrameStart
+    update(win, deltaTime)
     draw(win)
 
     glfw.swapBuffers(win)
